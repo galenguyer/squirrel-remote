5738e5e1c2bad8844248e2dcfee823dda2ff904a
Galen Guyer <galen@galenguyer.com>
2020-12-23 20:39:22 -0800

    dockerize that shit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.8-alpine
+LABEL maintainer="Galen Guyer <galen@galenguyer.com>"
+
+RUN apk add tzdata && \
+    cp /usr/share/zoneinfo/America/New_York /etc/localtime && \
+    echo 'America/New_York' > /etc/timezone && \
+    apk del tzdata
+
+WORKDIR /opt/app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python" "./squirrel.py"]
--- squirrel.py
+++ squirrel.py
@@ -1,19 +1,23 @@
 import json
-import sys
+import urllib.request
 import time
-import logging
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
-from pymongo import MongoClient
+import os
+from os.path import join, dirname
 
-conn_str = open('.mongo', 'r').read().strip()
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+conn_str = os.getenv('MONGO_URI')
 client = MongoClient(conn_str)
 db = client.dump1090
 
-def read_and_insert(event):
+def read_and_insert():
     data = {}
-    with open('aircraft.json', 'r') as fd:
-        raw = fd.read()
+    with urllib.request.urlopen("https://planes.galenguyer.com/data/aircraft.json") as url:
+        raw = url.read().decode()
         if len(raw.strip()) < 10:
             return
         data = json.loads(raw)
@@ -25,30 +29,5 @@ def read_and_insert(event):
         print(f'skipping insert for {data["now"]}, already stored')
 
 if __name__ == '__main__':
-    data = {}
-    with open('aircraft.json', 'r') as fd:
-        data = json.loads(fd.read())
-
-    if db.aircraft.find_one({'now': data['now']}) is None:
-        item = db.aircraft.insert_one(data)
-        print(f'inserted {data["now"]} with id {item.inserted_id}')
-    else:
-        print(f'skipping insert for {data["now"]}, already stored')
-    patterns = "*json"
-    ignore_patterns = ""
-    ignore_directories = True
-    case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-    my_event_handler.on_created = read_and_insert
-    my_event_handler.on_modified = read_and_insert
-    path = "."
-    go_recursively = True
-    my_observer = Observer()
-    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
-    my_observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        my_observer.stop()
-        my_observer.join()
+    while True:
+        read_and_insert()
